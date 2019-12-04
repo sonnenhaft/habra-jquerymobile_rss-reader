1fa1c2c0ba294275d24a8cccc30256e1f8e1b966
sonnenhaft <vladimir.sonnenhaft@gmail.com>
2019-12-04 17:16:43 +0800

    finally fixing rss reader with proper proxy, and saving functionality, sexiest ever

--- RssReaderPage.js
+++ RssReaderPage.js
@@ -1,16 +1,31 @@
-var savedUrl = localStorage.getItem('FeedUrl') || 'https://habr.ru/rss/best';
+const addFeedToDom = (title, description) => {
+    const forgetButton = $('.forgetButton').clone().click(function () {
+        const savedFeeds = JSON.parse(localStorage.getItem('feeds') || '[]');
+        savedFeeds.splice(savedFeeds.indexOf(({title: t}) => t === title), 1)
+        localStorage.setItem('feeds', JSON.stringify(savedFeeds));
+        const targetBlock = $('[data-title="' + title + '"]');
+        targetBlock.fadeOut('slow', () => targetBlock.remove());
+        $('[data-save-button="' + title + '"]').show().closest('h4').removeClass('hidden-button');
+        return false;
+    });
 
-$.get('https://zebrazobrazubra.000webhostapp.com/?url=' + savedUrl, function (data) {
-    var rssMemory = new RssMemory();
-    rssMemory.initSavedFeeds();
+    $('#savedFeeds').append($('<div>', {
+        'data-title': title,
+        'data-collapsed-icon': "arrow-r",
+        'data-expanded-icon': "arrow-d"
+    }).append(
+        $('<h4>', {text: title}).append(forgetButton),
+        $('<p>').html(description)
+    ).collapsible());
+}
 
-    const feeds = $(data).find('item').toArray().map(item => ({
-        content: $(item).find('description').html().toString().replace('<![CDATA[', '').replace(']]&gt;', ''),
-        image: $(item).find('url').text(),
-        title: $(item).find('title').text().replace('<![CDATA[', '').replace(']]>', '')
-    }));
+const savedFeeds = JSON.parse(localStorage.getItem('feeds') || '[]');
+savedFeeds.forEach(feed => {
+    addFeedToDom(feed.title, feed.description);
+})
 
-    $('#feeds').collapsibleset().html(feeds.map(({content, title}, index) => `<div data-role="collapsible" text-id="${index}">
+const setFeeds = feeds => {
+    $('#feeds').collapsibleset().html(feeds.map(({description, title}, index) => `<div data-role="collapsible" text-id="${index}">
                     <h4${localStorage.getItem(title) ? 'class="hidden-button"' : ''}>
                         ${title}
                          <a href="index.html" data-role="button"
@@ -21,17 +36,35 @@ $.get('https://zebrazobrazubra.000webhostapp.com/?url=' + savedUrl, function (da
             </div>`).join('')).collapsibleset('refresh');
 
     $('.saveButton').click(function () {
-        let {title, content} = feeds[$(this).parent().parent().parent().attr('text-id')];
+        const feed = feeds[$(this).parent().parent().parent().attr('text-id')];
         $(this).fadeOut('slow', function () {
             $(this).closest('h4').addClass('hidden-button');
         });
-        rssMemory.rememberFeed(title, content);
+
+        const savedFeeds = JSON.parse(localStorage.getItem('feeds') || '[]');
+        savedFeeds.push(feed);
+        localStorage.setItem('feeds', JSON.stringify(savedFeeds))
+        addFeedToDom(feed.title, feed.description)
         return false;
     });
 
-    $('h4').one('click', function () {
-        $(this).parent().find('p').html(feeds[$(this).parent().attr('text-id')].content)
+    const savedFeeds = JSON.parse(localStorage.getItem('feeds') || '[]');
+    savedFeeds.forEach(({title}) => {
+        const idx = feeds.findIndex(({title: t}) => title === title);
+        $(`[text-id="${idx}"] h4`).addClass('hidden-button');
+    });
+
+    $('#feeds h4').one('click', function () {
+        $(this).parent().find('p').html(feeds[$(this).parent().attr('text-id')].description)
     })
+}
+
+const savedUrl = localStorage.getItem('CurrentRssUrl') || 'https://habr.ru/rss/best';
+$('#CurrentRssUrl').val(savedUrl);
+$.get('https://zebrazobrazubra.000webhostapp.com/?url=' + savedUrl, ({title, item}) => {
+    $('#RssChannelTitle').text(title);
+    $('title').text(title);
+    setFeeds(item);
 }).fail(function () {
     // $.mobile.changePage("#rss_error", {transition: 'pop', role: 'dialog'});
     $.mobile.changePage("#offline_error", {transition: 'pop', role: 'dialog'});
